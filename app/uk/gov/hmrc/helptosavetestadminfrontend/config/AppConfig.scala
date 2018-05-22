344c5d7247b0c582d1055643812e4abb294b91e8
Jackie1988 <jacquelinehubbard88@gmail.com>
2018-05-22 11:34:00 +0100

    HTS:911 Removed unused code

--- app/uk/gov/hmrc/helptosavetestadminfrontend/config/AppConfig.scala
+++ app/uk/gov/hmrc/helptosavetestadminfrontend/config/AppConfig.scala
@@ -28,12 +28,5 @@ class AppConfig @Inject()(val runModeConfiguration: Configuration, environment: 
 
   private def loadConfig(key: String) = runModeConfiguration.getString(key).getOrElse(throw new Exception(s"Missing configuration key: $key"))
 
-  private val contactHost = runModeConfiguration.getString(s"contact-frontend.host").getOrElse("")
-  private val contactFormServiceIdentifier = "MyService"
-
   lazy val assetsPrefix = loadConfig(s"assets.url") + loadConfig(s"assets.version")
-  lazy val analyticsToken = loadConfig(s"$env.google-analytics.token")
-  lazy val analyticsHost = loadConfig(s"$env.google-analytics.host")
-  lazy val reportAProblemPartialUrl = s"$contactHost/contact/problem_reports_ajax?service=$contactFormServiceIdentifier"
-  lazy val reportAProblemNonJSUrl = s"$contactHost/contact/problem_reports_nonjs?service=$contactFormServiceIdentifier"
 }
