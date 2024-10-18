0d905422c70986385f0b2904d6da82002ccb84ce
johno1985 <1640705+johno1985@users.noreply.github.com>
2024-10-18 11:44:23 +0100

    DLS-10984: Update dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -9,12 +9,12 @@ object AppDependencies {
     ws,
     "uk.gov.hmrc"       %% s"bootstrap-frontend-$playVersion" % hmrcBootstrapVersion,
     "uk.gov.hmrc"       %% "totp-generator"                   % "0.25.0",
-    "org.typelevel"     %% "cats-core"                        % "2.10.0",
+    "org.typelevel"     %% "cats-core"                        % "2.12.0",
     "uk.gov.hmrc"       %% s"play-frontend-hmrc-$playVersion" % "8.5.0"
   )
 
   def test(scope: String = "test"): Seq[ModuleID] = Seq(
     "uk.gov.hmrc"       %% s"bootstrap-test-$playVersion"  % hmrcBootstrapVersion % scope,
-    "org.mockito"       %% "mockito-scala"                 % "1.17.30"            % scope,
+    "org.mockito"       %% "mockito-scala"                 % "1.17.37"            % scope
   )
 }
