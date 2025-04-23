77823879f45586298a4a55ffe35d6bf542caaeb7
johno1985 <1640705+johno1985@users.noreply.github.com>
2025-04-23 13:58:06 +0100

    DLS-11585: Update cats-core to 2.13.0

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -8,7 +8,7 @@ object AppDependencies {
   val compile: Seq[ModuleID] = Seq(
     hmrc            %% s"bootstrap-frontend-$playVersion" % hmrcBootstrapVersion,
     hmrc            %% "totp-generator"                   % "0.26.0",
-    "org.typelevel" %% "cats-core"                        % "2.12.0",
+    "org.typelevel" %% "cats-core"                        % "2.13.0",
     hmrc            %% s"play-frontend-hmrc-$playVersion" % "11.13.0"
   )
 
