d6ef2c454d6ef755e50b0bfadd3011930c9b34c9
johno1985 <1640705+johno1985@users.noreply.github.com>
2025-04-23 12:50:33 +0100

    DLS-11585: Updated play-frontend-hmrc to 11.13.0

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -10,7 +10,7 @@ object AppDependencies {
     "uk.gov.hmrc"       %% s"bootstrap-frontend-$playVersion" % hmrcBootstrapVersion,
     "uk.gov.hmrc"       %% "totp-generator"                   % "0.25.0",
     "org.typelevel"     %% "cats-core"                        % "2.12.0",
-    "uk.gov.hmrc"       %% s"play-frontend-hmrc-$playVersion" % "8.5.0"
+    "uk.gov.hmrc"       %% s"play-frontend-hmrc-$playVersion" % "11.13.0"
   )
 
   def test(scope: String = "test"): Seq[ModuleID] = Seq(
